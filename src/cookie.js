a0105f602b83d9f7ae63e16f4929032affeaa09a
Sutan Nst <sutan.gnst@gmail.com>
2019-03-17 10:16:13 +0700

    v 1.4.0

--- src/cookie.js
+++ src/cookie.js
@@ -16,8 +16,8 @@ export const get = key => {
   const cookies = document.cookie.split(';')
   for(let i = 0; i < cookies.length; i++) {
     let cookie = cookies[i]
-    while (cookie.charAt(0) == ' ') cookie = cookie.substring(1, cookie.length)
-    if (cookie.indexOf(key) == 0) return JSON.parse(cookie.substring(key.length, cookie.length))
+    while (cookie.charAt(0) === ' ') cookie = cookie.substring(1, cookie.length)
+    if (cookie.indexOf(key) === 0) return JSON.parse(cookie.substring(key.length, cookie.length))
   }
   return null
 }
